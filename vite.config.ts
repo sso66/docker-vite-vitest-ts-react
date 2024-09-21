651f4ff518112c456b4e6786fa21f1f8f22db0f1
Stephen <sun.stephen.oo@gmail.com>
2024-09-21 11:35:53 -0400

    add react-router-dom package

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,6 @@ export default defineConfig({
 
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
-    port: 5173 // you can replace this port with an port
+    port: 5173 // you can replace this port with any port
   }
 })
