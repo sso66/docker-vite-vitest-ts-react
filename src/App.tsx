83dc4f7810f94c42deb6dc47a82686dae153c212
Stephen <sun.stephen.oo@gmail.com>
2024-09-22 13:00:37 -0400

    updated eslint and prettier setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
+import { useState } from "react";
 import "./App.css";
-// import { useState } from 'react'
-
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 // import Blog from './Blog'
 
@@ -17,7 +16,7 @@ import "./App.css";
 // import AxiosApi from './api/AxiosApi'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -28,26 +27,26 @@ function App() {
         </h3>
       </header>
       <main className="vitest">
-        {/* <div>
-          <a href='https://vitejs.dev' target='_blank'>
-            <img src={viteLogo} className='logo' alt='Vite logo' />
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
           </a>
-          <a href='https://react.dev' target='_blank'>
-            <img src={reactLogo} className='logo react' alt='React logo' />
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
           </a>
-        </div> */}
+        </div>
 
-        {/* <div className=''>
-          <button onClick={() => setCount(count => count + 1)}>
+        <div className="">
+          <button onClick={() => setCount((count) => count + 1)}>
             count is {count}
           </button>
           <p>
             Edit <code>src/App.tsx</code> and save to test HMR
           </p>
-        </div> */}
-        {/* <p className='read-the-docs'>
+        </div>
+        <p className="read-the-docs">
           Click on the Vite and React logos to learn more
-        </p> */}
+        </p>
         {/* <Classroom name="Saint Paul High School" /> */}
 
         {/* <div className="navbar">
